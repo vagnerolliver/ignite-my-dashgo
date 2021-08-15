90905c5b403b0a963c898ac4586c22c00b1ae83c
Vagner Oliveira <vagner.oliveira@dextra-sw.com>
2021-08-15 10:34:53 -0300

    chore: create Input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack } from '@chakra-ui/react'
 
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -19,39 +20,8 @@ export default function Home() {
         flexDir="column"
       > 
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">
-              Email
-            </FormLabel>
-            <Input
-              name="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bg="gray.900"
-              variant="filled"
-              _hover={{
-                bg:"gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>
-          
-          <FormControl>
-            <FormLabel htmlFor="password">
-              Password
-            </FormLabel>
-            <Input
-              name="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bg="gray.900" 
-              variant="filled"
-              _hover={{
-                bg:"gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="email" label="Email" /> 
+          <Input name="password" type="password" label="Password" /> 
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink">entrar</Button>

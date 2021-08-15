1c8a84241c88c3b6c8b7641f2499464a493d6f51
Vagner Oliveira <vagner.oliveira@dextra-sw.com>
2021-08-15 11:12:24 -0300

    chore: create Header - partial 1

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,53 @@
+import { Flex, Text, Input, Icon } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri'
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      align="center"
+      h="20"
+      mt="4"
+      maxWidth={1480}
+      px="6"
+      w="100%"
+    >
+      <Text
+        fontSize="3xl"
+        fontWeight="bold"
+        letterSpacing="tight"
+        w="64"
+      >
+        My Dashgo
+        <Text as="span" color="pink.500" ml="1">.</Text>
+      </Text>
+
+      <Flex 
+        as="label"
+        flex="1"
+        alignSelf="center"
+        borderRadius="full"
+        bg="gray.800"
+        color="gray.200"
+        maxWidth={400}
+        ml="6"
+        position="relative"
+        py="4"
+        px="8"
+      > 
+      <Input
+        color="gray.50"
+        variant="unstyled"
+        placeholder="Buscar na Plataforma"
+        mr="4"
+        _placeholder={{
+          color:"gray.400"
+        }} 
+        px="4"
+      />    
+
+      <Icon as={RiSearchLine} fontSize="28" />
+    </Flex>
+    </Flex>
+  )
+}
